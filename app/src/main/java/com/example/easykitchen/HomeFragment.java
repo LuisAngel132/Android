f674870e4d498697454f43117efff3b5f5516a73
Your Name <you@example.com>
2021-11-15 19:10:14 -0600

    first commit

---
+++ app/src/main/java/com/example/easykitchen/HomeFragment.java
@@ -0,0 +1,18 @@
+package com.example.easykitchen;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment() {
+
+}
